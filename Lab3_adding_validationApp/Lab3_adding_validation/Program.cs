e9baab6fd7259b4957a70c541458526e2eae095e
Kevin Pink <kevin.pink@4s-llc.com>
2022-05-05 16:28:57 -0400

    corrections for remote

--- Lab3_adding_validationApp/Lab3_adding_validation/Program.cs
+++ Lab3_adding_validationApp/Lab3_adding_validation/Program.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Lab3_adding_validation
 {
-    internal class Program
+    internal class 
+        Program
     {
             static void Main()
             {
